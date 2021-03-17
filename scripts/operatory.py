f27a2f23aa903c47ee93132402ad2210480f589b
Raf87-beep <72692634+Raf87-beep@users.noreply.github.com>
2021-03-17 13:19:23 +0100

    commit na zakonczenie modulu 4, bez pracy domowej

---
+++ scripts/operatory.py
@@ -0,0 +1,30 @@
+suma = 1 + 2
+print(suma)
+
+odejmowanie = 2 - 1
+print(odejmowanie)
+
+reszta = 11 % 3
+print(reszta)
+
+doKwadratu = 10 ** 2
+print(doKwadratu)
+
+doSzescianu = 10 ** 2
+print(doSzescianu)
+
+print('zmienna ' + 'jest ' + 'super ')
+
+zmienna = 'no siema ' * 10
+print(zmienna)
+
+parzyste = [2,4,6,8]
+nieparzyste = [1,3,5,7]
+
+naturalne = parzyste + nieparzyste
+print(naturalne)
+
+print(1>1)
+print(1<2)
+print(1==2)
+print(1!=2)
---
+++ scripts/typyIZmienne.py
@@ -0,0 +1,24 @@
+print(5==0)
+print(5==5)
+
+number = 1
+number1 = 2
+print(number + number1)
+
+#listy
+list = [1,2,3]
+print(list)
+print(list[0])
+
+list2 = ['Ala','ma','kota',9]
+print(list2)
+print(list2[0:3])
+
+# uzywanie '' i ""
+text = 'czesc, jestem "Rafal"'
+print(text)
+
+# słowniki
+cities = {'city':['Warszawa', 'Gdansk','Bratyslawa']}
+print(cities['city'])
+print(cities['city'][2])
